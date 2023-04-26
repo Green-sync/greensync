0b4c206673c08f8c07ae22042956b1a87b91f62a
Mitch Chimwemwe Chanza <hello@mitch.guru>
2023-04-26 08:50:37 +0200

    feat: update application name

--- Packages/ui/src/components/Loader/LoaderComponent.tsx
+++ Packages/ui/src/components/Loader/LoaderComponent.tsx
@@ -3,7 +3,7 @@ export const LoaderComponent = ({ msg }: any) => {
     return <>
         <div className="flex flex-col justify-center items-center">
                 <span className="loader w-[70vw] h-[80vh]"></span>
-                <p className="text-green-600 text-2xl">{msg}</p>
+                <p className=" mt-6 text-green-600 text-2xl">{msg}</p>
         </div>
 
     </>
