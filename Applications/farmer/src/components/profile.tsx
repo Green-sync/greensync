522a666c644a3a45260c1854f23b1e1904e553ec
Mitch Chimwemwe Chanza <hello@mitch.guru>
2023-04-26 20:34:27 +0200

    feat: add todo

--- Applications/farmer/src/components/profile.tsx
+++ Applications/farmer/src/components/profile.tsx
@@ -2,7 +2,12 @@
 const profile = () => {
   return (
     <div>
-      
+      {/* 
+      TODO: implement profile component here 
+      assignees: @mitch1009
+      labels: frontend
+      milestones: 1
+      */}
     </div>
   )
 }
