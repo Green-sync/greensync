cd7e6a13e94f1ac13cb55b157a63616363ed2ca1
Daniel Mokoena <danielmduduzi88@gmail.com>
2023-04-27 23:51:38 +0200

    feat: added individual mutation, only missing chaining mutations

--- Backend/functions/src/modules/farmer/weather/schema/mutation.resolvers.ts
+++ Backend/functions/src/modules/farmer/weather/schema/mutation.resolvers.ts
@@ -1,4 +1,22 @@
+import { db } from "../../../../utils";
 
 export const WeatherMutationResolver = {
-   
+   //WeatherData Mutation
+   createWeatherData: async (_:any, args: { input:any }) => {
+    const newWeatherDataRef = await db.collection("weatherData").add(args.input);
+    const newWeatherDataSnapshot = await newWeatherDataRef.get();
+    return {
+      ...newWeatherDataSnapshot.data(),
+      weatherDataId: newWeatherDataSnapshot.id,
+    };
+  },
+  updateWeatherData: async (_:any, args: { weatherDataId:string, input: any }) => {
+    const weatherDataRef = db.collection("weatherData").doc(args.weatherDataId);
+    await weatherDataRef.update(args.input);
+    const updatedWeatherDataSnapshot = await weatherDataRef.get();
+    return {
+      ...updatedWeatherDataSnapshot.data(),
+      weatherDataId: updatedWeatherDataSnapshot.id,
+    };
+  },
 }
