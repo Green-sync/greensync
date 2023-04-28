cb75dfc87f86d5c0f84d81712d5d3ca47bf27a48
Mitch Chimwemwe Chanza <hello@mitch.guru>
2023-04-28 18:40:47 +0200

    feat(ui): add navigation component
    Co-authored-by: Dzuni-Cuthberts <Dzunisan@users.noreply.github.com>
    Co-authored-by: Dineo Mathibela <MathibelaD@users.noreply.github.com>

---
+++ Packages/ui/src/components/Navigation/NavigationComponent.tsx
@@ -0,0 +1,14 @@
+import { NavigationProps } from "./NavigationDto"
+import { NavigationSwitch } from "./NavigationSwitch"
+
+export const NavigationComponent = ({type,data}: NavigationProps) => {
+     
+    {/* // ref #17: Farmer with sidebar*/}
+    {/* Website Navigation ref #16 */}
+    {/* Marketplace  Navigation - redf #18 */}
+
+    return NavigationSwitch()[type](data)
+
+}
+
+export default NavigationComponent
