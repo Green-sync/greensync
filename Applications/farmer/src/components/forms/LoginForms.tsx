63c5f19ec0c5a2bf476434be6170d2ee3999c5a7
Dzuni-Cuthberts <mongwed@gmail.com>
2023-04-28 16:54:30 +0200

    login

--- Applications/farmer/src/components/forms/LoginForms.tsx
+++ Applications/farmer/src/components/forms/LoginForms.tsx
@@ -47,17 +47,17 @@ export default function LoginForms() {
 
     return (
       <>
-        <div className="flex min-h-full flex-1">
-          <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="flex min-h-full  flex-1">
+          <div className="flex  flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
             <div className="mx-auto w-full max-w-sm lg:w-96">
                 <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
                   Sign {signInType === "Login" ? " In" : " Up"} to your account
                 </h2>
 
-              <div className="mt-10">
-                <div>
+              <div className="mt-10 ">
+                <div className="">
 {/* =======================================================LOGIN==================================================================== */}
-                  <form id="custom,login" className={signInType == "Login" ?  "space-y-6" : 'hidden' }
+                  <form id="custom,login" className={signInType == "Login" ?  "space-y-6  p-4 border-slate-900" : 'hidden'}
                   onSubmit={authenticate} 
                   >
                     <div>
