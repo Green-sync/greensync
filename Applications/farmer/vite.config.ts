9598b5b7f1e5dcae016eb0c3d8a7c9df6e6874fb
Mitch chimwemwe Chanza <hello@mitch.guru>
2023-05-01 13:47:22 +0200

    fix(farmer): re-initialize vite

--- Applications/farmer/vite.config.ts
+++ Applications/farmer/vite.config.ts
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            external: [
-                "@heroicons/react/20/solid",
-                "@heroicons/react/24/outline",
-                "firebase/auth",
-                "@headlessui/react",
-                "localforage",
-                "match-sorter",
-                "react-router-dom",
-                "sort-by",
-                'react-router-dom'
-            ]
-        }
-    }
 })
