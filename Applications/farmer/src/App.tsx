d77cb522dbb649c49a771e2c25d657f0425250fb
Mitch chimwemwe Chanza <hello@mitch.guru>
2023-05-01 15:54:44 +0200

    fix(farmer): export default App

--- Applications/farmer/src/App.tsx
+++ Applications/farmer/src/App.tsx
@@ -2,7 +2,7 @@
 import './App.css'
 import {Loader} from "greensync-ui";
 const env = import.meta.env
-function App() {
+export default  function App() {
 const message = `Welcome to ${env.VITE_APP_ENV==="DEV"? "GreenSync Dev Platform": "GreenSync"}`
   return (
     <>
