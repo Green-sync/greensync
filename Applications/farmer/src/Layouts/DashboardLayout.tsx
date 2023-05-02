bd4dee5cf97fe53f6f117dc7860fa86a227b933a
Mitch chimwemwe Chanza <hello@mitch.guru>
2023-05-02 22:45:17 +0200

    fix: workflow

--- Applications/farmer/src/Layouts/DashboardLayout.tsx
+++ Applications/farmer/src/Layouts/DashboardLayout.tsx
@@ -2,7 +2,9 @@ import { NavigationComponent } from 'greensync-ui'
 import { DashBordNavigationStaticData } from '../components'
 const DashboardLayout = () => {
   return (
-    
+    /**
+     * Dashboard navigation Here
+     */
         <NavigationComponent type={'farmer'} data={DashBordNavigationStaticData}/>
    
   )
