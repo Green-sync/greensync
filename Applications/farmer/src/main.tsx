05b0125544f44f59c8fa3387bb610d4d304cd968
Mitch chimwemwe Chanza <hello@mitch.guru>
2023-05-03 08:27:53 +0200

    feat(client): app fix

--- Applications/farmer/src/main.tsx
+++ Applications/farmer/src/main.tsx
@@ -5,6 +5,6 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter><App/></BrowserRouter>
   </React.StrictMode>,
 )
