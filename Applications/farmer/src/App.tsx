e0718f114f4ee8a142328c4b42f0d9f595629191
Mitch chimwemwe Chanza <hello@mitch.guru>
2023-05-03 06:17:08 +0200

    feat(client): add navigation routes

--- Applications/farmer/src/App.tsx
+++ Applications/farmer/src/App.tsx
@@ -1,14 +1,16 @@
 
 import {Routes,Route} from 'react-router-dom'
 import { HomeLayout } from './Layouts';
+import { AuthPage } from './Pages/AuthPage';
+import DashboardLayout from './Layouts/DashboardLayout';
 
 export const App = () => {
 
   return <>
   <Routes>
     <Route path="/" element={<HomeLayout />} />
-    <Route path="/auth" element={<h1/>} />
-    <Route path="/dashboard" element={<h1/>} />
+    <Route path="/auth" element={<AuthPage/>} />
+    <Route path="/dashboard" element={<DashboardLayout/>} />
   </Routes>
   </>
 };
