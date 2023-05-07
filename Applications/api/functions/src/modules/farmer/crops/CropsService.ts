38647c99b1e737bda0c1c919cac1ba96ed727475
Daniel Mokoena <danielmduduzi88@gmail.com>
2023-05-07 20:18:51 +0200

    fix: farm & crops adding.

--- Applications/api/functions/src/modules/farmer/crops/CropsService.ts
+++ Applications/api/functions/src/modules/farmer/crops/CropsService.ts
@@ -2,17 +2,31 @@ import { db } from "../../../utils";
 
 export class CropsService {
   static async addCrop(crop: any) {
-        try {
+        // try {
           
-          await db.collection("Crops").add(crop)
-          return {
-            message: "Crop details successfully added", success: true,
-          };
-        } catch (error) {
-          return {
-            message: `${error} failed to add crop details`, success: false,
-          };
-        }
+        //   await db.collection("Crops").add(crop)
+        //   return {
+        //     message: "Crop details successfully added", success: true,
+        //   };
+        // } catch (error) {
+        //   return {
+        //     message: `${error} failed to add crop details`, success: false,
+        //   };
+        // }
+
+        const cropRef = await db.collection('Crops').add({
+          ...crop,
+          userId: "1d1d12345" //context.uid
+        });
+        // Return the newly created crop with its ID
+        return {
+          message: "Crop details successfully added", success: true,
+          id: cropRef.id, cropRef
+        };
+      } catch(error: any) {
+        return {
+          message: `${error} failed to add crop details`, success: false,
+        };
       }
 
 }
--- Applications/api/functions/src/modules/farmer/farm/farmService.ts
+++ Applications/api/functions/src/modules/farmer/farm/farmService.ts
@@ -2,18 +2,33 @@ import { db } from "../../../utils";
 
 export class FarmService {
 
-    static async addFarm(farm: any) {
-        try {
-          
-            await db.collection("Farms").add(farm)
-          return {
-            message: "Farm details successfully added", success: true,
-          };
-        } catch (error) {
-          return {
-            message: `${error} failed to add farm details`, success: false,
-          };
-        }
-      }
+  static async addFarm(farm: any) {
+    // try {
+
+    //     await db.collection("Farms").add(farm)
+    //   return {
+    //     message: "Farm details successfully added", success: true,
+    //   };
+    // } catch (error) {
+    //   return {
+    //     message: `${error} failed to add farm details`, success: false,
+    //   };
+    // }
+
+    // Create a new document in the "farms" collection with the specified user ID and farm details
+    const farmRef = await db.collection('Farms').add({
+      ...farm,
+      userId: "1d1d12345" //context.uid
+    });
+    // Return the newly created farm with its ID
+    return {
+      message: "Farm details successfully added", success: true,
+      id: farmRef.id, farmRef
+    };
+  } catch(error: any) {
+    return {
+      message: `${error} failed to add farm details`, success: false,
+    };
+  }
 
 }
