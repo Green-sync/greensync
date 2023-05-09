be14721a6aed05ea3e5c356378fbd6aa30960f1e
LANGABOM <batilelanga@hotmail.com>
2023-05-09 13:23:42 +0200

    Registration changes

--- Packages/ui/src/components/RegistrationForms/RegistrationForm.tsx
+++ Packages/ui/src/components/RegistrationForms/RegistrationForm.tsx
@@ -1,4 +1,4 @@
-import { InputTypes } from ".";
+import { ISelectDto, InputTypes } from ".";
 import { FarmFormStaticData } from "./RegistrationDto/RegisterFormStaticData"
 
 
@@ -14,7 +14,7 @@ export const RegistrationFormComponent = () => {
                     <div className="w-4/5  bg-white border-b border-gray-200 md:p-8">
 
                         <form className="space-y-6">
-                           
+
 
                             {
                                 FarmFormStaticData.map((input, index) => {
@@ -22,36 +22,32 @@ export const RegistrationFormComponent = () => {
                                         <div key={index}>
 
                                             {
-                                                input.type === 'text' ? <input type={input.type} name={input.name} id={input.id} className=" border-b border-lime-600 text-lime-900 text-sm  focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-white dark:border-lime-500  dark:text-white" placeholder={input.placeholder} required={true} /> :
+                                                input.type === 'text' ? <input type={input.type} name={input.name} id={input.id} className=" border-b border-grey-600 text-lime-900 text-sm  block w-full p-2.5 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-lime-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" placeholder={input.placeholder} required={true} /> :
                                                     input.type === 'radio' ? <>
-                                                        {input.isSelect?.map((data: any, index: any) => {
+
+                                                        <div key={index} className="relative h-10 w-72 min-w-[200px] mt-4">
+                                                            <select className="peer mt-2 h-full w-full rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-blue-gray-200 empty:!bg-lime-600 focus:border-2 focus:border-lime-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
+                                                                <label id={input.id} className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-lime-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-lime-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-lime-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                                                                    {input.label}
+                                                                </label>
+                                                                {input.isSelect?.map((data: ISelectDto, index: any) => {
+
+                                                                    return (
 
 
-                                                            <>
-                                                                <ul key={index} className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                                                                    <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                                                        <div className="flex items-center pl-3">
-                                                                            <input
-                                                                                id={data.name}
-                                                                                type="radio"
-                                                                                value={data.value}
-                                                                                name={input.name}
-                                                                                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                                                                            />
+                                                                        <>
 
-                                                                            <label
-                                                                                htmlFor={data.name}
-                                                                                className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                                                                            >
-                                                                                {data.name}
-                                                                            </label>
-                                                                        </div>
-                                                                    </li>
 
-                                                                </ul>
-                                                            </>
+                                                                            <option value={data.value}>{data.name}</option>
 
-                                                        })}
+
+
+
+                                                                        </>
+                                                                    )
+                                                                })}
+                                                            </select>
+                                                        </div>
                                                     </> :
                                                         <textarea name={input.name} id={input.id} placeholder={input.placeholder} className="bg-gray-50 border h-28 dark:bg-inherit border-lime-300 text-gray-900 text-sm  focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-white dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required={true} />}
 
