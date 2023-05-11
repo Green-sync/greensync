eb8a34fc75294b20df2a898a2cc707df9b1a72da
MathibelaD <dineomathibela161@gmail.com>
2023-05-12 01:18:28 +0200

    .......

--- Packages/ui/src/components/Weather/WeatherComponent.tsx
+++ Packages/ui/src/components/Weather/WeatherComponent.tsx
@@ -3,7 +3,6 @@ import { WeatherIcon } from "./WearherDto/WeatherIcon";
 import { weekData } from "./WearherDto/weatherStaticData";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import "./Weather.scss"; 
-import { IWeatherData } from "./WearherDto/WeatherDto";
 
 
 export const WeatherComponent = () => {
@@ -17,7 +16,6 @@ export const WeatherComponent = () => {
 
   const filteredData = weekData.filter((item) =>
     item.location.toLowerCase() === searchTerm.toLowerCase()
-    // item.location.toLowerCase().includes(searchTerm.toLowerCase())
   );
   useEffect(()=>{
     setSearchWeather(filteredData)
@@ -25,20 +23,20 @@ export const WeatherComponent = () => {
   console.log(searchWeather);
   return (
     <>
-      <div className="container p-6 bg-green-900 text-white">
+      <div className="container p-6 bg-lime-900 text-white">
         <div className="flex flex-wrap md:grid-cols-2 gap-4">
           {/* <!-- First column --> */}
           <div className="flex-grow md:w-auto">
             <div className="grid grid-cols-1 justify-items-center ">
               <div
                 onClick={() => setOpen(!open)}
-                className="pr-1 flex justify-between border-1 border-gray-600 w-1/3 rounded-lg bg-gray-600 items-center"
+                className="pr-1 flex justify-between border-1 border-gray-600 w-1/3 rounded-lg bg-lime-700 bg-opacity-10 items-center"
               >
                 <input
                   type="text"
                   placeholder="Search  for location"
                   onChange={handleSearch}
-                  className="outline-none p-2 bg-gray-600 w-full rounded-lg"
+                  className="outline-none p-2 bg-lime-700 bg-opacity-10 w-full rounded-lg"
                 />
                 <span className="flex">
                   <MagnifyingGlassIcon className="w-5 pointer-events-none" />
@@ -47,7 +45,7 @@ export const WeatherComponent = () => {
              
               {
                         open && <>
-                        <div className=" rounded bg-gray-600 w-1/3 m-2">
+                        <div className=" rounded bg-green-900 bg-opacity-10 w-1/3 m-2">
                             <p className="m-2 text-lg text-gray-500">My Locations</p>
                             {searchWeather.map((v) => {
                                 return <>
@@ -61,21 +59,20 @@ export const WeatherComponent = () => {
 
             <div className="flex justify-between">
               <div>
-                <p className="text-white text-2xl font-extrabold">
-                  {/* {searchWeather.location} */}
+                <p className="text-white text-4xl font-extrabold">
+                  {searchWeather[0]?.location}
                 </p>
                 <p className="text-gray-100 font-thin text-sm">
                   Chance of rain 0 %
                 </p>
-                <p className="text-gray-100 font-medium p-4">{31}&#176;</p>
-              </div>
-              <div className="h-40 w-40">
-                {/* <img src="https://camo.githubusercontent.com/f45ac6a3081bef820e779dbaa0765cae602ba0bf7030749059b1a2234f6dd1f4/68747470733a2f2f626d63646e2e6e6c2f6173736574732f776561746865722d69636f6e732f76322e302f6c696e652f636c6561722d6461792e737667" /> */}
-                {/* {searchWeather[0].weekWeather[3].icon} */}
+                <div className="flex space-x-0">
+                 {searchWeather[0]?.weekWeather[0].icon} 
+                <p className="text-gray-100 font-medium text-2xl">{31}&#176;</p>
+                </div>
               </div>
             </div>
 
-            <div className=" border-gray-400 mb-2 bg-gray-400 rounded-sm pb-4">
+            <div className=" border-gray-400 mb-2 bg-lime-700 bg-opacity-10 rounded-md pb-4">
               <p className="m-2 font-extrabold text-lg">Today's forecast</p>
               <div className="flex flex-wrap">
                 {searchWeather.map((value) => {
@@ -109,10 +106,10 @@ export const WeatherComponent = () => {
                 })}
               </div>
             </div>
-            <div className=" border-gray-400 mb-2 bg-gray-400 rounded-sm pb-4">
+            <div className=" border-gray-400 mb-2 bg-lime-700 bg-opacity-10 rounded-md border-1 pb-4">
               <div className="flex justify-between">
                 <p className="m-2 font-extrabold text-lg">Air conditions</p>
-                <button className="bg-green-800 rounded m-2 p-1">
+                <button className="bg-lime-800 rounded m-2 p-1">
                   See more
                 </button>
               </div>
@@ -138,12 +135,12 @@ export const WeatherComponent = () => {
           </div>
           {/* ===========================================SECOND COLUMN============================================================= */}
           <div className="w-full md:w-auto">
-            <div className="border-gray-400 mb-2 bg-gray-400 rounded-md pb-4 md:w-96 ">
-              <p className="m-2 font-semibold">7 days forecast</p>
-              {searchWeather.map((value, index) => {
+            <div className="border-gray-400 mb-4 bg-lime-700 bg-opacity-10 rounded-md pb-4 md:w-96 ">
+              <p className="m-4 font-semibold">7 days forecast</p>
+              {searchWeather.map((value) => {
                 return (
                   <>
-                    {value.weekWeather.map((d, i) => {
+                    {value.weekWeather.map((d) => {
                       return (
                         <>
                         <div className="flex justify-between items-center ml-4 mr-4">
