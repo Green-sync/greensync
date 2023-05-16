a73996475acfdb00eeabe8a6977bfb21e775d992
Dzunisan <mongwed@gmail.com>
2023-05-16 22:12:13 +0200

    stringify selected crops and livestock

--- Packages/ui/src/components/Farm/CropsComponent.tsx
+++ Packages/ui/src/components/Farm/CropsComponent.tsx
@@ -1,49 +1,58 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
-const crops = [
-  {
+interface ICrops{
+  id:number,
+  title:string,
+  description:string,
+  status:string,
+  image:string
+}
+
+const crops:ICrops[] = [
+  {id:1,
     title: 'Wheat',
     description: 'Wheat is a cereal grain that is commonly cultivated for its seed.',
     status: 'Heading',
     image: "https://images.pexels.com/photos/326082/pexels-photo-326082.jpeg?auto=compress&cs=tinysrgb&w=600"
   },
-  {
+  {id:2,
     title: 'Corn',
     description: 'Corn is a popular crop that is used for both food and fuel.',
     status: 'Tasseling',
     image: "https://images.pexels.com/photos/547264/pexels-photo-547264.jpeg?auto=compress&cs=tinysrgb&w=600"
   },
-  {
+  {id:3,
     title: 'Soybeans',
     description: 'Soybeans are a legume that is commonly used for animal feed and processed food products.',
     status: 'Podding',
     image: "https://images.pexels.com/photos/3735174/pexels-photo-3735174.jpeg?auto=compress&cs=tinysrgb&w=600"
   },
-  {
+  {id:4,
     title: 'Potatoes',
     description: 'Potatoes are a root vegetable that is commonly used in many dishes around the world.',
     status: 'Tuber Initiation',
     image: 'https://images.pexels.com/photos/2286776/pexels-photo-2286776.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:5,
     title: 'Cotton',
     description: 'Cotton is a soft, fluffy staple fiber that is used to make textiles and clothing.',
     status: 'Square',
     image: 'https://images.pexels.com/photos/6280177/pexels-photo-6280177.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:6,
     title: 'Sorghum',
     description: 'Sorghum is a cereal grain that is commonly used for animal feed and ethanol production.',
     status: 'Booting',
     image: 'https://media.istockphoto.com/id/1347198038/photo/biofuel-and-food-sorghum-plantation-industry-field-of-sweet-sorghum-stalk-and-seeds-millet.jpg?b=1&s=612x612&w=0&k=20&c=EgRRmx3maR1aKsOlLHVYJSji3ekRlFmSFKICmPmCAv8='
   },
-  {
+  {id:7,
     title: 'Tomatoes',
     description: 'Tomatoes are a fruit that is commonly used in cooking and can be eaten raw or cooked.',
     status: 'Green Fruit',
     image: 'https://images.pexels.com/photos/533280/pexels-photo-533280.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:8,
     title: 'Peanuts',
     description: 'Barley is a cereal grain that is commonly used in beer and whisky production.',
     status: 'Booting',
@@ -53,6 +62,18 @@ const crops = [
 ];
 
 export const CropsComponent = () => {
+  const [selectedCrops,setSelectedCrops]=useState<ICrops[]>([]);
+
+  const handleSelectedCrops =(CropsItem:ICrops)=>{
+    const itemIndex = selectedCrops.findIndex(item=>item.title===CropsItem.title);
+    if (itemIndex !== -1){
+      setSelectedCrops(prevSelectedCrops =>[...prevSelectedCrops.slice(0, itemIndex), ...prevSelectedCrops.slice(itemIndex + 1)])
+    }
+    else{
+      setSelectedCrops(prevSelectedCrops=>[...prevSelectedCrops,CropsItem])
+    }
+  }
+
   return (
 
     <div>
@@ -74,7 +95,8 @@ export const CropsComponent = () => {
           {crops.map((crop, index) => (
             <div
               key={index}
-              className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+              className={`relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400 ${selectedCrops.some(item => item.id === crop.id) ? 'bg-green-400 text-white' : ''}`}   
+              onClick={()=> handleSelectedCrops(crop)}
             >
               <div className="flex-shrink-0">
                 <img className="h-10 w-10 rounded-full" src={crop.image} alt="" />
@@ -90,6 +112,9 @@ export const CropsComponent = () => {
           ))}
         </div>
       </div>
+      <pre>
+        {JSON.stringify(selectedCrops, null, 2)}
+      </pre>
 
     </div>
   );
--- Packages/ui/src/components/Farm/LivestockComponent.tsx
+++ Packages/ui/src/components/Farm/LivestockComponent.tsx
@@ -1,32 +1,41 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from 'react';
 
 
-const livestock = [
-  {
+interface ILivestockItem {
+  title: string;
+  description: string;
+  status: string;
+  image: string;
+  id?:number
+}
+
+const livestock: ILivestockItem[] = [
+  {id:1,
     title: 'Cattle',
     description: 'Cattle are domesticated mammals that are raised for meat, dairy products, and leather.',
     status: 'Growth Stage: Adult',
     image: 'https://images.pexels.com/photos/735968/pexels-photo-735968.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:2,
     title: 'Pigs',
     description: 'Pigs are intelligent, social animals that are raised for meat and other products.',
     status: 'Growth Stage: Piglet',
     image: 'https://images.pexels.com/photos/14534682/pexels-photo-14534682.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:3,
     title: 'Sheep',
     description: 'Sheep are domesticated mammals that are raised for wool, meat, and milk.',
     status: 'Growth Stage: Lamb',
     image: 'https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:4,
     title: 'Chickens',
     description: 'Chickens are domesticated birds that are raised for their meat and eggs.',
     status: 'Growth Stage: Chick',
     image: 'https://images.pexels.com/photos/2255459/pexels-photo-2255459.jpeg?auto=compress&cs=tinysrgb&w=600'
   },
-  {
+  {id:5,
     title: 'Goats',
     description: 'Goats are domesticated mammals that are raised for their milk, meat, and wool.',
     status: 'Growth Stage: Kid',
@@ -36,18 +45,26 @@ const livestock = [
 
 
 export const LivestockComponent = () => {
-  const handleCardClick = (livestock) => {
-    console.log(livestock);
+  const [selectedLivestocks, setSelectedLivestocks] = useState<ILivestockItem[]>([]);
+
+  const handleCardClick = (livestockItem: ILivestockItem) => {
+    const itemIndex = selectedLivestocks.findIndex(item => item.title === livestockItem.title);
+    if (itemIndex !== -1) {
+      setSelectedLivestocks(prevSelectedCards => [...prevSelectedCards.slice(0, itemIndex), ...prevSelectedCards.slice(itemIndex + 1)]);
+    } else {
+      setSelectedLivestocks(prevSelectedCards => [...prevSelectedCards, livestockItem]);
+    }
   };
 
   return (
     <div>
+      
       <div className="pb-10">
         <h1 className="text-2xl flex items-center justify-center p-3 rounded-md text-white bg-yellow-600 font-bold mb-4">My Livestock</h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-4">
           <div
             className="relative flex bg-lime-900 items-center space-x-3 rounded-lg border border-gray-300 px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
-            onClick={() => handleCardClick('Add Livestock')}
+            // onClick={() => handleCardClick('Add Livestock')}
           >
             <div className="flex-shrink-0">
               <PlusCircleIcon className="h-10 w-10 text-white" />
@@ -62,8 +79,8 @@ export const LivestockComponent = () => {
           {livestock.map((livestockItem, index) => (
             <div
               key={index}
-              className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
-              onClick={() => handleCardClick(livestockItem)}
+              className={`relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400 ${selectedLivestocks.some(item => item.id === livestockItem.id) ? 'bg-green-400 text-white' : ''}`}   
+                         onClick={() => handleCardClick(livestockItem)}
             >
               <div className="flex-shrink-0">
                 <img className="h-10 w-10 rounded-full" src={livestockItem.image} alt="" />
@@ -79,6 +96,9 @@ export const LivestockComponent = () => {
           ))}
         </div>
       </div>
+      <pre>
+      <pre>{JSON.stringify(selectedLivestocks, null, 2)}</pre>
+      </pre>
     </div>
   );
 };
