3e9c84ee3f0e8246cbc325dbcf317580e51d58ea
Daniel Mokoena <danielmduduzi88@gmail.com>
2023-05-17 19:28:31 +0200

    fix: create user

--- Applications/api/functions/src/modules/farmer/profile/profileServices.ts
+++ Applications/api/functions/src/modules/farmer/profile/profileServices.ts
@@ -1,28 +1,123 @@
 
 import { db } from "../../../utils/firbase.config";
+import { UserDetails } from "../../auth/contextDto";
 import { ProfileInput } from "./schema";
 
 
 export class ProfileService {
-    static async user(profile: any, user: any) {
+    static async user(profile: any, user: UserDetails) {
+
+            const { userId, name, surname, email, PhoneNumbers, password, website, photoURL, homeAddress, farms } = profile;
+            
+            try {
+              const profileRef = db.collection('User').doc(user.uid);
+              const profileData = {
+                userId,
+                name,
+                surname,
+                email,
+                PhoneNumbers,
+                password,
+                website,
+                photoURL,
+                homeAddress,
+                farms: farms.map((farm: { name: any; location: any; size: any; description: any; crops: { name: any; quantity: any; }[]; livestocks: { name: any; count: any; }[]; }) => ({
+                  name: farm.name,
+                  location: farm.location,
+                  size: farm.size,
+                  description: farm.description,
+                  crops: farm.crops ? farm.crops.map((crop: { name: any; }) => ({
+                    name: crop.name,
+                    
+                  })) : [],
+                  livestocks: farm.livestocks ? farm.livestocks.map((livestock: { name: any; }) => ({
+                    name: livestock.name,
+                    
+                  })) : [],
+                })),
+              };
+              
+              await profileRef.set(profileData);
+              
+              return profileRef.id;
+            
+
+        
+        
 
 
 
         // Create a new document in the "farms" collection with the specified user ID and farm details
-         await db.collection('User').doc(user.uid).set({
-          ...profile,
-        });
+        //   await db.collection('User').doc(user.uid).set({
+        //   ...profile,
+          
+
+        // });
+
+
+
+        
+
+
+
         // Return the newly created farm with its ID
         
-        return {
-          message: "User details successfully added", success: true,
+      //   return {
+      //     message: "User details successfully added", success: true,
          
-        };
+      //   };
       } catch(error: any) {
         return {
           message: `${error} failed to add user details`, success: false,
         };
+
+    
+   
+      
+      //   const {
+      //     userId,
+      //     name,
+      //     surname,
+      //     email,
+      //     PhoneNumbers,
+      //     password,
+      //     website,
+      //     photoURL,
+      //     homeAddress,
+      //     farms,
+      //   } = profile;
+  
+      //   try {
+      //     const profileRef = db.collection('profiles').doc();
+      //     const profileData = {
+      //       userId,
+      //       name,
+      //       surname,
+      //       email,
+      //       PhoneNumbers,
+      //       password,
+      //       website,
+      //       photoURL,
+      //       homeAddress,
+      //       farms: farms.map((farm: { crops: any; livestocks: any; }) => ({
+      //         ...farm,
+      //         crops: farm.crops ? farm.crops : [],
+      //         livestocks: farm.livestocks ? farm.livestocks : [],
+      //       })),
+      //     };
+  
+      //     await profileRef.set(profileData);
+  
+      //     return profileRef.id;
+      //   } catch (error) {
+      //     console.error('Error creating profile:', error);
+      //     throw new Error('Failed to create profile');
+      //   }
+        }
       }
+
+      
+    
 
 
       static async editUser(profileId: string, updates: ProfileInput) {
