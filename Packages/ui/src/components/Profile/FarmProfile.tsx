2cac89c8d929eb3fb2a19f921e07f0695bb24cf8
MathibelaD <dineomathibela161@gmail.com>
2023-05-18 14:45:14 +0200

    feat: Add profile for farm

--- Packages/ui/src/components/Profile/FarmProfile.tsx
+++ Packages/ui/src/components/Profile/FarmProfile.tsx
@@ -52,31 +52,31 @@ const crops = [
     {
       title: 'Cattle',
       description: 'Cattle are domesticated mammals that are raised for meat, dairy products, and leather.',
-      status: 'Growth Stage: Adult',
+      status: '15',
       image: 'https://images.pexels.com/photos/735968/pexels-photo-735968.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
     {
       title: 'Pigs',
       description: 'Pigs are intelligent, social animals that are raised for meat and other products.',
-      status: 'Growth Stage: Piglet',
+      status: '42',
       image: 'https://images.pexels.com/photos/14534682/pexels-photo-14534682.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
     {
       title: 'Sheep',
       description: 'Sheep are domesticated mammals that are raised for wool, meat, and milk.',
-      status: 'Growth Stage: Lamb',
+      status: '2',
       image: 'https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
     {
       title: 'Chickens',
       description: 'Chickens are domesticated birds that are raised for their meat and eggs.',
-      status: 'Growth Stage: Chick',
+      status: '82',
       image: 'https://images.pexels.com/photos/2255459/pexels-photo-2255459.jpeg?auto=compress&cs=tinysrgb&w=600'
     },
     {
       title: 'Goats',
       description: 'Goats are domesticated mammals that are raised for their milk, meat, and wool.',
-      status: 'Growth Stage: Kid',
+      status: '17',
       image: 'https://images.pexels.com/photos/3193155/pexels-photo-3193155.jpeg?auto=compress&cs=tinysrgb&w=600'
     }
   ];
@@ -185,8 +185,8 @@ export const FarmProfile = () => {
                 <div className="min-w-0 flex-1">
                   <a href="#" className="focus:outline-none">
                     <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="text-sm font-medium text-gray-900">{livestock.title}</p>
-                    <p className="truncate text-sm text-gray-500">{livestock.status}</p>
+                    <p className="text-lg font-medium text-gray-900">{livestock.title}</p>
+                    <p className="truncate text-sm text-gray-500">{livestock.status} {livestock.title}</p>
                   </a>
                 </div>
               </div>
