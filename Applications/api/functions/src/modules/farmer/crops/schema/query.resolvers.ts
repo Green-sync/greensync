dfb88c7ce87a1c838868df06ea56e1da2dfe9213
Daniel Mokoena <danielmduduzi88@gmail.com>
2023-05-18 17:12:20 +0200

    fix: removed comments

--- Applications/api/functions/src/modules/farmer/crops/schema/query.resolvers.ts
+++ Applications/api/functions/src/modules/farmer/crops/schema/query.resolvers.ts
@@ -1,9 +1,6 @@
 import { CropsService } from "../CropsService";
 
 export const CropsQueryResolver = {
-    // getCrop: (_:unknown, __:unknown, context: {user:any, db: any}) => { 
-    //     return context? {message:`Welcome to the crops world ${context}`}: {message:'Unauthorized'};
-    // },
     getCrop: async (_parent: unknown, __:any, context: { user: any }) => {
     
         return CropsService.getCrop(context.user);
